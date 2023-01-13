8f9c45b9f6cd40e085bdd18fd25a8264c7b1bedc
Prithvi Varma Nallapuraju <98509310+prithvivarma-nallapuraju@users.noreply.github.com>
2023-01-13 10:31:53 +0530

    added all the required class extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        orange: "hsl(25, 97%, 53%)",
+        lightGray: "hsl(217, 12%, 63%)",
+        mediumGray: "hsl(216, 12%, 54%)",
+        darkBlue: "hsl(213, 19%, 18%)",
+        veryDarkBlue: "hsl(216, 12%, 8%)",
+      },
+      fontFamily: {
+        overPass: ["Overpass", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
