32ad0e32bd399da9c40cd2ccb7feaf7b703e6e7c
Prithvi Varma Nallapuraju <98509310+prithvivarma-nallapuraju@users.noreply.github.com>
2023-01-13 17:46:10 +0530

    completed final structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
     },
     extend: {
       colors: {
-        orange: "hsl(25, 97%, 53%)",
-        lightGray: "hsl(217, 12%, 63%)",
-        mediumGray: "hsl(216, 12%, 54%)",
-        darkBlue: "hsl(213, 19%, 18%)",
-        veryDarkBlue: "hsl(216, 12%, 8%)",
+        orangeCustom: "hsl(25, 97%, 53%)",
+        lightGrayCustom: "hsl(217, 12%, 63%)",
+        mediumGrayCustom: "hsl(216, 12%, 54%)",
+        darkBlueCustom: "hsl(213, 19%, 18%)",
+        veryDarkBlueCustom: "hsl(216, 12%, 8%)",
       },
       fontFamily: {
         overPass: ["Overpass", "sans-serif"],
